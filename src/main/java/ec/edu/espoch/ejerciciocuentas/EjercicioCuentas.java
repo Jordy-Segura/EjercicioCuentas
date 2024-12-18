1214399eb2e1a3b7551d302a9c4b2f14b593ebd2
Jordy-Segura <marcelo2005jmsp@gmail.com>
2024-12-18 14:05:00 -0500

    rama jordy creada

---
+++ src/main/java/ec/edu/espoch/ejerciciocuentas/EjercicioCuentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.ejerciciocuentas;
+
+/**
+ *
+ * @author SO-LAB1-PC19
+ */
+public class EjercicioCuentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
