4b8be8b829655ffd9496fe10cac6bbf24d849132
Jordy-Segura <marcelo2005jmsp@gmail.com>
2024-12-18 14:21:36 -0500

    3

--- src/main/java/ec/edu/espoch/ejerciciocuentas/clases/Cuentas.java
+++ src/main/java/ec/edu/espoch/ejerciciocuentas/clases/Cuentas.java
@@ -9,5 +9,56 @@ package ec.edu.espoch.ejerciciocuentas.clases;
  * @author SO-LAB1-PC19
  */
 public class Cuentas {
+    protected float saldo;
+    protected int consignacion=0;
+    protected int retiros=0;
+    protected float tasaAnual;
+    protected float comisionMensual=0;
+
+    public Cuentas(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
+    
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getConsignacion() {
+        return consignacion;
+    }
+
+    public void setConsignacion(int consignacion) {
+        this.consignacion = consignacion;
+    }
+
+    public int getRetiros() {
+        return retiros;
+    }
+
+    public void setRetiros(int retiros) {
+        this.retiros = retiros;
+    }
+
+    public float getTasaAnual() {
+        return tasaAnual;
+    }
+
+    public void setTasaAnual(float tasaAnual) {
+        this.tasaAnual = tasaAnual;
+    }
+
+    public float getComisionMensual() {
+        return comisionMensual;
+    }
+
+    public void setComisionMensual(float comisionMensual) {
+        this.comisionMensual = comisionMensual;
+    }
+    
     
 }
